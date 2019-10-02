4be93069726c352762b4fc211193bae8053e11da
nassilio <hubmattfac@fac.com>
2019-10-02 11:02:04 +0200

    ex1 map resolved

--- map/ex1.js
+++ map/ex1.js
@@ -22,6 +22,9 @@ En sortie: [
  */
 
 function getStringsLength(strings) {
+  return strings.map (string => {
+    return string + ' contains ' + string.length + ' characters'
+  })
 }
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
